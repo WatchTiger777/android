48180f852f325cce02ee2c88a295362e1c4992fe
WatchTiger777 <939642978@qq.com>
2022-11-11 15:03:59 +0800

    实现了长按弹出菜单的功能，但还没完善编辑删除等功能

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
     List<News> mNewsList = new ArrayList<>();
 
     @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    public void CreateMenu(Menu menu)
+    {
+        int groupID = 0;
+        int order = 0;
+        int[] itemID = {1,2,3};
+        for(int i=0;i<itemID.length;i++)
+        {
+            switch(itemID[i])
+            {
+                case 1:
+                    menu.add(groupID, itemID[i], order, "编辑");
+                    break;
+                case 2:
+                    menu.add(groupID, itemID[i], order, "删除");
+                    break;
+                case 3:
+                    menu.add(groupID, itemID[i], order, "清空");
+                default:
+                    break;
+            }
+        }
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -32,13 +72,20 @@ public class MainActivity extends AppCompatActivity {
             news.pngId= R.drawable.ic_launcher_foreground;
             mNewsList.add(news);
         }
-        mMyAdapter = new MyAdapter();
+        mMyAdapter = new MyAdapter(mNewsList,this);
         mRecyclerView.setAdapter(mMyAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(layoutManager);
     }
 
-    class MyAdapter extends RecyclerView.Adapter<MyViewHoder> {
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHoder> {
+
+        private int position;
+        private Context mContext;
+        private List<News> mList;
+        public MyAdapter(List<News> fruitList, Context mContext){this.mContext = mContext;this.mList=fruitList;}
+        public int getContextMenuPosition() { return position; }
+        public void setContextMenuPosition(int position) { this.position = position; }
 
         @NonNull
         @Override
@@ -60,19 +107,31 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return mNewsList.size();
         }
-    }
 
-    class MyViewHoder extends RecyclerView.ViewHolder {
-        TextView mTitleTv;
-        TextView mTitleContent;
-        ImageView mTitlePng;
+        class MyViewHoder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
+            TextView mTitleTv;
+            TextView mTitleContent;
+            ImageView mTitlePng;
 
-        public MyViewHoder(@NonNull View itemView) {
-            super(itemView);
-            mTitleTv = itemView.findViewById(R.id.textView);
-            mTitleContent = itemView.findViewById(R.id.textView2);
-            mTitlePng = itemView.findViewById(R.id.imageView);
+            public MyViewHoder(@NonNull View itemView) {
+                super(itemView);
+                mTitleTv = itemView.findViewById(R.id.textView);
+                mTitleContent = itemView.findViewById(R.id.textView2);
+                mTitlePng = itemView.findViewById(R.id.imageView);
+                itemView.setOnCreateContextMenuListener(this);
+            }
+
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                //注意传入的menuInfo为null
+                News mSelectModelUser = mNewsList.get(getContextMenuPosition());
+                Log.i("UserAdapter", "onCreateContextMenu: "+getContextMenuPosition());
+                menu.setHeaderTitle(mSelectModelUser.title);
+                ((MainActivity)mContext).CreateMenu(menu);
+            }
         }
     }
-}
+    }
+
+
 
