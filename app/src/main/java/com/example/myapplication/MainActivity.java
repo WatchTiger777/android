7b8ca4a8b55d3fbf7fce4b6922edde98bf7179a8
WatchTiger777 <939642978@qq.com>
2022-11-11 14:37:07 +0800

    长按还没实现，但是列表图片可以显示

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.myapplication;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.recyclerview);
-        registerForContextMenu(mRecyclerView);
         // 构造一些数据
         for (int i = 0; i < 50; i++) {
             News news = new News();
@@ -44,50 +38,8 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(layoutManager);
     }
 
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case 1:
-                Toast.makeText(this, "edit", Toast.LENGTH_SHORT).show();
-                break;
-            case 2:
-                Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
-        }
-        return super.onContextItemSelected(item);
-    }
-
-
-    public void CreateMenu(Menu menu)
-    {
-        int groupID = 0;
-        int order = 0;
-        int[] itemID = {1,2};
-
-        for(int i=0;i<itemID.length;i++)
-        {
-            switch(itemID[i])
-            {
-                case 1:
-                    menu.add(groupID, itemID[i], order, "编辑");
-                    break;
-                case 2:
-                    menu.add(groupID, itemID[i], order, "删除");
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
     class MyAdapter extends RecyclerView.Adapter<MyViewHoder> {
 
-        private int position;
-        private List<News> mList;
-        public int getContextMenuPosition() { return position; }
-        public void setContextMenuPosition(int position) { this.position = position; }
         @NonNull
         @Override
         public MyViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -97,24 +49,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onViewRecycled(@NonNull MyViewHoder holder) {
-            holder.itemView.setOnLongClickListener(null);
-            super.onViewRecycled(holder);
-        }
-
-        @Override
         public void onBindViewHolder(@NonNull MyViewHoder holder, int position) {
             News news = mNewsList.get(position);
             holder.mTitleTv.setText(news.title);
             holder.mTitleContent.setText(news.content);
             holder.mTitlePng.setImageResource(news.pngId);
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    setContextMenuPosition(holder.getLayoutPosition());
-                    return false;
-                }
-            });
         }
 
         @Override
@@ -128,21 +67,11 @@ public class MainActivity extends AppCompatActivity {
         TextView mTitleContent;
         ImageView mTitlePng;
 
-
         public MyViewHoder(@NonNull View itemView) {
             super(itemView);
             mTitleTv = itemView.findViewById(R.id.textView);
             mTitleContent = itemView.findViewById(R.id.textView2);
             mTitlePng = itemView.findViewById(R.id.imageView);
-            itemView.setOnLongClickListener(this);
-        }
-
-        @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-            //注意传入的menuInfo为null
-            News mSelectModelUser = mList.get(getContextMenuPosition());
-            menu.setHeaderTitle(mSelectModelUser.getUserName());
-            ((UserActivity)mContext).CreateMenu(menu);
         }
     }
 }
